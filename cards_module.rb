4cb5a238c609d202026bbd8c49b1e5a4f0286b9a
Denys Podobrii <denys.podobrii@gmail.com>
2022-11-02 17:45:48 +0300

    create fill card pack method

---
+++ cards_module.rb
@@ -0,0 +1,19 @@
+module Cards
+  attr_accessor :card_pack
+
+  CARD_SUITE = ["+", "<3", "<>", "^"]
+  CARD_VALUES = {
+    "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7,
+    "8" => 8, "9" => 9, "10" => 10, "J" => 10, "Q" => 10, "K" => 10, "A" => 11
+  }
+
+  def fill_card_pack
+    @card_pack = []
+    CARD_SUITE.each do |suite|
+      CARD_VALUES.each do |value, cost|
+        card_pack << Card.new(value, suite, cost)
+      end
+    end
+    @card_pack
+  end
+end
