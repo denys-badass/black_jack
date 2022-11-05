ab143f9b3ecec23a81b4209912489a4222377ff8
Denys Podobrii <denys.podobrii@gmail.com>
2022-11-05 16:16:45 +0300

    modified player class

--- player.rb
+++ player.rb
@@ -1,6 +1,14 @@
 class Player
+  attr_accessor :money, :hand_cards
+  attr_reader :name
+
   def initialize(name)
     @name = name
     @money = 100
+    @hand_cards = []
+  end
+
+  def player_point(hand_cards)
+    puts hand_cards.select(&:cost)
   end
 end
