a26268b64c7a38837191690b9499b7a521224ef1
Denys Podobrii <denys.podobrii@gmail.com>
2022-11-05 20:29:17 +0200

    Create action menu method

--- interface_module.rb
+++ interface_module.rb
@@ -29,6 +29,7 @@ module Interface
     player_info(@diler)
     player_info(@user)
     show_points(@user)
+    action_menu
   end
 
   def player_info(player)
@@ -49,5 +50,21 @@ module Interface
 
   def show_points(player)
     puts "#{player.name} has #{player.points} points"
+  end
+
+  def action_menu
+    menu = {
+      1 => {:title => "Skip", :method => :diler_game},
+      2 => {:title => "Add card", :method => :add_card},
+      3 => {:title => "Open cards", :method => :open_cards}
+    }
+    puts "=" * 30
+    puts "What do you want to do?: "
+    menu.each { |key, value| puts "#{key} - #{value[:title]}" }
+    send(menu[gets.to_i][:method], player)
+  end
+
+  def diler_game
+    
   end
 end
